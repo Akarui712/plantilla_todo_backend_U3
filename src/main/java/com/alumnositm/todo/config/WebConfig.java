e527916ddb9bb6b88d8c3f27fcbf3a6bcef553e8
Akarui712 <a21490574@itmexicali.edu.mx>
2025-11-20 16:44:27 -0800

    xxx

--- src/main/java/com/alumnositm/todo/config/WebConfig.java
+++ src/main/java/com/alumnositm/todo/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     //@Value("${cors.allowed.origins:http://localhost:3000,http://localhost:5173}")
-    @Value("${cors.allowed.origins}")
+    @Value("${CORS_ALLOWED_ORIGINS}")
     private String allowedOrigins;
 
     @Override
