d4aa2954d93d2e8039db8f059b0e9fde73bce4d9
Akarui712 <a21490574@itmexicali.edu.mx>
2025-11-20 18:34:03 -0800

    new changes

--- src/main/java/com/alumnositm/todo/config/WebConfig.java
+++ src/main/java/com/alumnositm/todo/config/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins.split(","))
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
